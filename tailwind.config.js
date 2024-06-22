13406bc21de1b77f7c5e7f562e4e80fbadb69911
nourassili <nour.assili@amaltheafs.com>
2024-06-22 13:36:39 -0400

    Initial commit with resolved submodule issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],  
+  // This pattern matches all .html and .ts files within the src directory and all its subdirectories.
+  // we selected which files we want to style
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
